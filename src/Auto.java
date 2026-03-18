545f1ea7246da63fec662ecf756eb5d20835e94e
Ain Tamm <aintamm@ut.ee>
2026-03-18 14:32:44 +0200

    inputid ja auto klass

---
+++ src/Auto.java
@@ -0,0 +1,50 @@
+public class Auto {
+    private String mark;
+    private String mudel;
+    private int aasta;
+    private int labiSoit;
+
+    public Auto(String mark, String mudel, int aasta, int labiSoit) {
+        this.mark = mark;
+        this.mudel = mudel;
+        this.aasta = aasta;
+        this.labiSoit = labiSoit;
+    }
+
+    public
+
+
+
+
+    public String getMark() {
+        return mark;
+    }
+
+    public String getMudel() {
+        return mudel;
+    }
+
+    public int getAasta() {
+        return aasta;
+    }
+
+    public int getLabiSoit() {
+        return labiSoit;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public void setMudel(String mudel) {
+        this.mudel = mudel;
+    }
+
+    public void setAasta(int aasta) {
+        this.aasta = aasta;
+    }
+
+    public void setLabiSoit(int labiSoit) {
+        this.labiSoit = labiSoit;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,13 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args){
-        String garaaziNimi = JOptionPane.showInputDialog("Sisesta oma garaazi nimi: ");
+        String mark = JOptionPane.showInputDialog("Sisesta auto mark: ");
+        String mudel = JOptionPane.showInputDialog("Sisesta auto mudel: ");
+        int aasta = Integer.parseInt(JOptionPane.showInputDialog("Sisesta auto väljalaskeaasta: "));
+        int ls = Integer.parseInt(JOptionPane.showInputDialog("Sisesta oma auto läbisõit: "));
+
+        Auto esimene = new Auto(mark, mudel, aasta, ls);
+        System.out.println(esimene.getAasta());;
+
     }
 }
