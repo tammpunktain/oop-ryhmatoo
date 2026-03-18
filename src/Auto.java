5d608b43b61ece25abdcd3e1f31f5dc749ee97ae
Ain Tamm <aintamm@ut.ee>
2026-03-18 15:35:35 +0200

    kytusekoguse lisamine

--- src/Auto.java
+++ src/Auto.java
@@ -3,20 +3,49 @@ public class Auto {
     private String mudel;
     private int aasta;
     private int labiSoit;
+    private double kytust; //liitrites
+    private double kytusekulu; // L/100km
 
-    public Auto(String mark, String mudel, int aasta, int labiSoit) {
+    public Auto(String mark, String mudel, int aasta, int labiSoit, double kytust, double kytusekulu) {
         this.mark = mark;
         this.mudel = mudel;
         this.aasta = aasta;
         this.labiSoit = labiSoit;
+        this.kytust = kytust;
+        this.kytusekulu = kytusekulu;
     }
 
     public void soida(int km){
         labiSoit+=km;
+        kytust -= kytusekulu * (km/100.0);
     }
 
 
+    @Override
+    public String toString() {
+        return "Auto{" +
+                "mark='" + mark + '\'' +
+                ", mudel='" + mudel + '\'' +
+                ", aasta=" + aasta +
+                ", labiSoit=" + labiSoit +
+                '}';
+    }
 
+    public void setKytust(double kytust) {
+        this.kytust = kytust;
+    }
+
+    public void setKytusekulu(double kytusekulu) {
+        this.kytusekulu = kytusekulu;
+    }
+
+    public double getKytust() {
+        return kytust;
+    }
+
+    public double getKytusekulu() {
+        return kytusekulu;
+    }
 
     public String getMark() {
         return mark;
--- src/Main.java
+++ src/Main.java
@@ -6,20 +6,26 @@ public class Main {
         String mudel = JOptionPane.showInputDialog("Sisesta auto mudel: ");
         int aasta = Integer.parseInt(JOptionPane.showInputDialog("Sisesta auto väljalaskeaasta: "));
         int ls = Integer.parseInt(JOptionPane.showInputDialog("Sisesta oma auto läbisõit: "));
+        double kytust = Double.parseDouble(JOptionPane.showInputDialog("Sisesta oma kütusekogus liitrites: "));
+        double kytusekulu = Double.parseDouble(JOptionPane.showInputDialog("Sisesta oma kytusekulu 100 km kohta liitrites: "));
 
-        Auto esimene = new Auto(mark, mudel, aasta, ls);
+        Auto esimene = new Auto(mark, mudel, aasta, ls, kytust, kytusekulu);
 
+        System.out.println("Labisoit enne sõitu on: " + esimene.getLabiSoit());
+        System.out.println("kütusekogus enne sõitu on: " + esimene.getKytust());
 
         Object[] variandid = new String[]{"Sõida", "Tangi", "remondi"};
         Object valitud = JOptionPane.showInputDialog(null,
                 "Vali tegevus", "Sisestus",
                 JOptionPane.INFORMATION_MESSAGE, null,
                 variandid, variandid[0]);
-        System.out.println("Labisoit enne sõitu on: " + esimene.getLabiSoit());;
+
         if (valitud == "Sõida"){
             int soidetud = Integer.parseInt(JOptionPane.showInputDialog("Kui palju sõita soovid?"));
             esimene.soida(soidetud);
         } else System.out.println("vale valik");
+
         System.out.println("Labisoit parast sõitu on: " + esimene.getLabiSoit());
+        System.out.println("Kütusekogus parast sõitu on: " + esimene.getKytust());
     }
 }
