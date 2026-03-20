71f59a1bf06bcf4ea324265aeba2bceb89bf868c
kerdotimak <kerdotimak@gmail.com>
2026-03-20 16:10:52 +0200

    Loodi garaaži klass.

---
+++ src/Garaaž.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+
+public class Garaaž {
+    private ArrayList<Auto> autod;
+    private String nimi;
+
+    public Garaaž(String nimi) {
+        this.nimi = nimi;
+        this.autod = new ArrayList<>();
+    }
+
+    public void lisaAuto(Auto auto) {
+        autod.add(auto);
+        System.out.println("Auto " + auto.getMark() + " " + auto.getMudel() + " lisati garaaži: "+nimi+" .");
+    }
+
+    public void kuvaNimekiri() {
+        if (autod.isEmpty()) {
+            System.out.println("Garaaz on tühi.");
+            return;
+        }
+        for (int i = 0; i < autod.size(); i++) {
+            System.out.println((i + 1) + ". " + autod.get(i));
+        }
+    }
+
+    public Auto valiAuto(int indeks) {
+        if (indeks >= 1 && indeks <= autod.size()) {
+            return autod.get(indeks - 1);
+        }
+        System.out.println("Vale indeks");
+        return null;
+    }
+
+    public void tangi(Auto auto, double liitrid) {
+        auto.setKytust(auto.getKytust() + liitrid);
+        System.out.println("Tankitud " + liitrid + "L. Uus kütusekogus: " + auto.getKytust() + "L");
+    }
+
+    public void soitmine(Auto auto, int km) {
+        double vajaminevKytus = auto.getKytusekulu() * (km / 100.0);
+        if (auto.getKytust() < vajaminevKytus) {
+            System.out.println("Pole piisavalt kütust! Vajad " + vajaminevKytus + "L, aga autos on " + auto.getKytust() + "L");
+            return;
+        }
+        auto.soida(km);
+        System.out.println("Sõideti " + km + "km.");
+        System.out.println("Uus läbisõit: " + auto.getLabiSoit() + "km");
+        System.out.println("Järelejäänud kütus: " + auto.getKytust() + "L");
+    }
+
+    public int getAutodeArv() {
+        return autod.size();
+    }
+
+    public String getNimi() {
+        return nimi;
+    }
+}
